383619dd2518c846963bec55d190ff6d6d732a54
ritviks <ritvik_shah@sfu.ca>
2022-02-27 21:39:45 -0800

    altered db schema, added price data

---
+++ populate_price.py
@@ -0,0 +1,42 @@
+import os
+import sqlite3
+import alpaca_trade_api as tradeapi
+
+from dotenv import load_dotenv
+
+#import env vars
+load_dotenv()
+api_key = os.getenv("api_key")
+secret_api_key = os.getenv("secret_api_key")
+domain = os.getenv("domain")
+
+connection = sqlite3.connect('app.db')
+
+connection.row_factory = sqlite3.Row
+cursor = connection.cursor()
+cursor.execute("SELECT id, symbol, name FROM stock")
+
+rows = cursor.fetchall() #fetch price data
+
+symbols = []
+stock_dict = {}
+for row in rows:
+    symbol = row['symbol']
+    symbols.append(symbol)
+    stock_dict[symbol] = row['id']
+
+api = tradeapi.REST(api_key,secret_api_key, base_url=domain)
+
+chunk_size = 200 #loop in terms of chunks of 200
+for i in range(0,len(symbols), chunk_size): #loop through all symbols in steps of 200
+    symbol_chunk = symbols[i:i+chunk_size] 
+
+    barsets = api.get_barset(symbol_chunk, 'day') #get barsets for each chunk by day
+ 
+    for symbol in barsets:
+        print(f"processing symbol {symbol}")    
+        for bar in barsets[symbol]:
+            stock_id = stock_dict[symbol] #insert data into db
+            cursor.execute("INSERT INTO stock_price (stock_id, date, open, high, low, close, volume) VALUES (?,?,?,?,?,?,?)", (stock_id, bar.t.date(), bar.o, bar.h, bar.l, bar.c, bar.v))
+
+connection.commit()
---
+++ populate_stock.py
@@ -0,0 +1,39 @@
+import os
+import sqlite3
+import alpaca_trade_api as tradeapi
+
+from dotenv import load_dotenv
+
+#scheduling command: 58 15 * * * /Users/ritvikshah/Downloads/Code/trading-app/populate_db.py >> populate.log 2>&1
+
+#import env vars
+load_dotenv()
+api_key = os.getenv("api_key")
+secret_api_key = os.getenv("secret_api_key")
+domain = os.getenv("domain")
+
+connection = sqlite3.connect('app.db')
+connection.row_factory = sqlite3.Row
+
+cursor = connection.cursor()
+
+cursor.execute("SELECT symbol, name FROM stock") # select all stocks
+
+rows = cursor.fetchall()
+symbols = [row['symbol'] for row in rows] # print a list of all symbols
+
+#connect to api
+api = tradeapi.REST(api_key,secret_api_key, base_url=domain)
+assets = api.list_assets() #get list of symbols
+
+for asset in assets: #loop through and print all assets 
+    try:
+        if asset.status == 'active' and asset.tradable and asset.symbol not in symbols:
+            print("asdas")
+            print(f"Added a new stock {asset.symbol} {asset.name}")
+            cursor.execute("INSERT INTO stock (symbol, name) VALUES (?, ?)", (asset.symbol, asset.name))
+    except Exception as e:
+        print(asset.symbol)
+        print(e)
+
+connection.commit()
